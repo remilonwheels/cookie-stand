76f5456d8b95418feda55e4455048322b7c8012a
Remil Marzan <remilonwheels@gmail.com>
2016-12-07 14:40:10 -0800

    code cleanup

--- app.js
+++ app.js
@@ -145,6 +145,4 @@ new Store('Seattle Center', 11, 38, 3.7);
 new Store('Capitol Hill', 20, 38, 2.3);
 new Store('Alki', 2, 16, 4.6);
 
-createTableHeader();
-createTableRows();
-createTableFooter();
+updateTable();
