baaec4800239db7bfdb3a73d84ad0eb18278cae3
Remil Marzan <remilonwheels@gmail.com>
2016-12-07 14:22:48 -0800

    added number validation on add form fields

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var allStores = [];
-var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var totalCookiesByHour = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var allStoresCookieTotal = 0;
 
@@ -115,6 +115,10 @@ function handleAddStore(event) {
 
   if (!storeName || !minCust || !maxCust || !avgOrder) {
     return alert('Please fill-in all boxes');
+  }
+
+  if (isNaN(minCust) || isNaN(maxCust) || isNaN(avgOrder)) {
+    return alert('Min/max customer and avg order fields must be numbers');
   }
 
   new Store(storeName, minCust, maxCust, avgOrder);
